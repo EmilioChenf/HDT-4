85f45c1e250d87072a2c2b7dabf82eeabc83edf7
fatitaprogramando <nav24044@uvg.edu.gt>
2025-02-17 19:42:32 -0600

    solo comente cosas

---
+++ src/main/java/ht4/calculator/Calculator.java
@@ -0,0 +1,16 @@
+/*******************************************************************************
+* @author Fatima Navarro 24044
+* 
+* Universidad del Valle de Guatemala
+* Algoritmos y Estructura de Datos
+* Sección: 10
+* 
+* HT4 - Clases y Objetos con Java
+* Calculadora Infix/Postfix
+* 
+* Calculator.java
+* Esta clase implementa el patrón Singleton y se encarga de la conversión
+* y evaluación de expresiones matemáticas.
+* 
+* Fecha: 16/02/2025
+*******************************************************************************/
---
+++ src/main/java/ht4/calculator/Main.java
@@ -0,0 +1,15 @@
+/*******************************************************************************
+* @author Fatima Navarro 24044
+* 
+* Universidad del Valle de Guatemala
+* Algoritmos y Estructura de Datos
+* Sección: 10
+* 
+* HT4 - Clases y Objetos con Java
+* Calculadora Infix/Postfix
+* 
+* Main.java
+* Clase principal que ejecuta el programa y maneja la interacción con el usuario.
+* 
+* Fecha: 16/02/2025
+*******************************************************************************/
