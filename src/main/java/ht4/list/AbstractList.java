da17c506a257e5b1fcef465c4eccf4a730a15d9f
Artli22 <ludinlima2514@gmail.com>
2025-02-20 17:31:51 -0600

    commit de prueba abstract

---
+++ src/main/java/ht4/list/AbstractList.java
@@ -0,0 +1,15 @@
+package ht4.list;
+
+public abstract class AbstractList<T> implements IList<T> {
+    protected int size = 0; 
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+}
